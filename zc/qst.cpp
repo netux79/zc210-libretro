57bc492ba367bb3daae5c75154eff441acef2d17
Ernesto Ramirez <netux79@yahoo.com>
2021-07-11 08:26:24 -0500

    Workaround to issue with newer compilers with this code that if tie 'for" statement goes from 0 to 8 it generates the wrong sequence (in this case it creates an iteration that goes from 0 to 20 for some reason). Changing it yo go from 8 to 0 solves the problem. This issue just affected old 1.90 quests that generates the door combo data at qst load time.

--- zc/qst.cpp
+++ zc/qst.cpp
@@ -416,7 +416,10 @@ void reset_scr(int scr)
 
 int operator ==(DoorComboSet a, DoorComboSet b)
 {
-   for (int i = 0; i < 9; i++)
+   /* There is a certain bug in newer compilers with this code
+    * if the "for" statement go from 0 to 8. Changing it to backwards
+    * solves the issue. */
+   for (int i = 8; i >= 0; i--)
    {
       for (int j = 0; j < 6; j++)
       {
